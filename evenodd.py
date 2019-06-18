12abbf3cc4d7b2f4761dd1e90694d9cf57dca4f2
jeevanfrancisk <51698901+jeevanfrancisk@users.noreply.github.com>
2019-06-18 15:44:03 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,11 @@
+try:
+    a=int(input(""))
+    if a%2==0:
+        print("Even")
+    elif a%2!=0:
+        print("Odd")
+    else:
+        print("Invalid")
+except:
+    print("Invalid")
+    pass
