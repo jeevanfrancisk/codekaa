578bc6e6f617cf45bcfec001011280a8c7a4ba26
jeevanfrancisk <51698901+jeevanfrancisk@users.noreply.github.com>
2019-07-16 14:17:49 +0530

    Create duplicate number.py

---
+++ duplicate number.py
@@ -0,0 +1,14 @@
+N=int(input())
+a=input("")
+a1=list(a.split(" "))
+b=[]
+for i in range(0,len(a1)):
+  for j in range(i+1,len(a1)):
+    if(a1[i]==a1[j]):
+      if a1[i] not in b:
+        b.append(a1[i])
+if len(b)==0:
+  print("unique")
+else:
+c=" ".join(b)
+print(c)
