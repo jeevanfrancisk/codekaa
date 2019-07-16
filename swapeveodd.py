4e12ba349eb16e2f4828e500520df06ed09c43cc
jeevanfrancisk <51698901+jeevanfrancisk@users.noreply.github.com>
2019-07-16 12:10:11 +0530

    Update swapeveodd.py

--- swapeveodd.py
+++ swapeveodd.py
@@ -1,10 +1,9 @@
 a=input("")
-n=len(a)
-b=list(a)
-for i in range(0,n+1):
+a=list(a)
+for i in range(0,len(a)+1):
   if(i%2!=0):
-    b[i-1],b[i]=b[i],b[i-1]
+    a[i-1],a[i]=a[i],a[i-1]
 d=""
-for s in b:
+for s in a:
   d+=s
 print(d)
